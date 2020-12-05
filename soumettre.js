bde40ab96d42bacab8fff6fc8dde0550e18e689e
gabrielgaudreault <75510490+gabrielgaudreault@users.noreply.github.com>
2020-12-05 17:31:03 -0500

    Add files via upload

---
+++ soumettre.js
@@ -0,0 +1,107 @@
+var boutonSoumettre = document.getElementById("boutonSoumettre");
+boutonSoumettre.addEventListener("click", corriger);
+valueInitialBoutonSoumettre = boutonSoumettre.value;
+
+
+var choix1 = document.getElementById("choix1");
+var choix2 = document.getElementById("choix2");
+var choix3 = document.getElementById("choix3");
+choix1.checked = false;
+choix2.checked = false;
+choix3.checked = false;
+
+var textStatus = document.getElementById("textStatus");
+
+
+var bonneReponseIndex = 0;
+var bonneReponseArray = [
+    2, 
+    3,
+    1,
+    1,
+    2,
+    3,
+    2
+];
+
+function corriger() {
+    if (choix1.checked == true) {
+        if (bonneReponseArray[bonneReponseIndex] == 1) {
+            bonneReponse();
+        } else {
+            erreur();
+        }
+    }
+    else if (choix2.checked == true) {
+        if (bonneReponseArray[bonneReponseIndex] == 2) {
+            bonneReponse();
+        } else {
+            erreur();
+        }
+    }
+    else if (choix3.checked == true) {
+        if (bonneReponseArray[bonneReponseIndex] == 3) {
+            bonneReponse();
+        } else {
+            erreur();
+        }
+    }
+}
+
+function erreur() {
+    textStatus.innerHTML = "erreur";
+    textStatus.style.color = "red";
+    boutonSoumettre.value = valueInitialBoutonSoumettre;
+}
+function bonneReponse() {
+    textStatus.innerHTML = "bonne réponse";
+    textStatus.style.color = "green";
+    boutonSoumettre.value = "continuer";
+    boutonSoumettre.removeEventListener("click", corriger);
+    boutonSoumettre.addEventListener("click", continuer);   
+}
+
+
+var probleme = [
+    document.getElementById("probleme1"),
+    document.getElementById("probleme2"),
+    document.getElementById("probleme3"),
+    document.getElementById("probleme4"),
+    document.getElementById("probleme5"),
+    document.getElementById("probleme6"), 
+    document.getElementById("probleme7")
+];
+var indexProbleme = 0;
+document.getElementById("max").innerHTML = probleme.length;
+var problemeIndexDom = document.getElementById("problemeIndex");
+continuer(true);
+function continuer(premiereFois = false) {
+    textStatus.innerHTML = "";
+    if (premiereFois == true) {
+        probleme[0].style.display = "block";
+        for(i = 1; i < probleme.length; i++) {
+            probleme[i].style.display = "none";
+        }
+        return null;
+    }
+    probleme[indexProbleme].style.display = "none";
+    indexProbleme++;
+    if (probleme[indexProbleme] == null) {
+        textStatus.innerHTML = "Vous avez fait tous les problèmes";
+        textStatus.style.color = "green";
+        document.getElementById("form").style.display = "none";
+        return null
+    }
+    bonneReponseIndex++;
+    probleme[indexProbleme].style.display = "block";
+
+    problemeIndexDom.innerHTML = indexProbleme + 1;
+
+    boutonSoumettre.removeEventListener("click", continuer);
+    boutonSoumettre.addEventListener("click", corriger);
+    boutonSoumettre.value = valueInitialBoutonSoumettre;
+    
+    choix1.checked = false;
+    choix2.checked = false;
+    choix3.checked = false;
+}
