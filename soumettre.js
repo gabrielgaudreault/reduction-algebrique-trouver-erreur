2469fda4bc3890087c26222b2a10e98feb1c8935
gabrielgaudreault <75510490+gabrielgaudreault@users.noreply.github.com>
2020-12-12 18:54:17 -0500

    Add files via upload

--- soumettre.js
+++ soumettre.js
@@ -7,6 +7,16 @@ var choix1 = document.getElementById("choix1");
 var choix2 = document.getElementById("choix2");
 var choix3 = document.getElementById("choix3");
 var aucunChoix = document.getElementById("choixAucun");
+
+var label1 = document.getElementById("label1");
+var label2 = document.getElementById("label2");
+var label3 = document.getElementById("label3");
+var label4 = document.getElementById("label4");
+var label1textDefault = label1.innerHTML;
+var label2textDefault = label2.innerHTML;
+var label3textDefault = label3.innerHTML;
+var label4textDefault = label4.innerHTML;
+
 choix1.checked = false;
 choix2.checked = false;
 choix3.checked = false;
@@ -41,6 +51,19 @@ function choixCliquer() {
         return null;
     }
 
+    if (choix1.checked == true && choix1cliquer) {
+        choix1.checked = false;
+    }
+    else if (choix2.checked == true && choix2cliquer) {
+        choix2.checked = false;
+    }
+    else if (choix3.checked == true && choix3cliquer) {
+        choix3.checked = false;
+    }
+    else if (aucunChoix.checked == true && choix4cliquer) {
+        choix4.checked = false;
+    }
+
     textStatus.innerHTML = "";
 }
 
@@ -62,36 +85,54 @@ var bonneReponseArray = [
     3
 ];
 
+var choix1cliquer;
+var choix2cliquer;
+var choix3cliquer;
+var choix4cliquer;
+
 function corriger() {
-    if (choix1.checked == true) {
+    if (choix1.checked == true && !choix1cliquer) {
         if (bonneReponseArray[bonneReponseIndex] == 1) {
             bonneReponse();
         } else {
+            label1.style.color = "red";
+            label1.innerHTML += "&nbsp;-&nbsp;erreur"
             erreur();
+            choix1cliquer = true;
         }
     }
-    else if (choix2.checked == true) {
+    else if (choix2.checked == true && !choix2cliquer) {
         if (bonneReponseArray[bonneReponseIndex] == 2) {
             bonneReponse();
         } else {
+            label2.style.color = "red";
+            label2.innerHTML += "&nbsp;-&nbsp;erreur"
             erreur();
+            choix2cliquer = true;
         }
     }
-    else if (choix3.checked == true) {
+    else if (choix3.checked == true && !choix3cliquer) {
         if (bonneReponseArray[bonneReponseIndex] == 3) {
             bonneReponse();
         } else {
+            label3.style.color = "red";
+            label3.innerHTML += "&nbsp;-&nbsp;erreur"
             erreur();
+            choix3cliquer = true;
         }
     }
-    else if (aucunChoix.checked == true) {
+    else if (aucunChoix.checked == true && !choix4cliquer) {
+        label4.style.color = "red";
+        label4.innerHTML += "&nbsp;-&nbsp;erreur"
         erreur();
+        choix4cliquer = true;
     }
 
     else {
         textStatus.innerHTML = "vous n'avez pas entrez de réponse";
         textStatus.style.color = "wheat";
     }
+
 }
 
 var bonneReponseEntre = false;
@@ -100,6 +141,16 @@ function erreur() {
     textStatus.innerHTML = "erreur";
     textStatus.style.color = "red";
     boutonSoumettre.value = valueInitialBoutonSoumettre;
+    note--;
+    if (note < 0) {
+        note = 0;
+    }
+
+    choix1.checked = false;
+    choix2.checked = false;
+    choix3.checked = false;
+    aucunChoix.checked = false;
+
 }
 function bonneReponse() {
     textStatus.innerHTML = "bonne réponse";
@@ -124,6 +175,9 @@ var probleme = [
     document.getElementById("probleme10")
 ];
 
+var nbProbleme = probleme.length;
+var note = nbProbleme;
+
 var indexProbleme = 0;
 document.getElementById("max").innerHTML = probleme.length;
 var problemeIndexDom = document.getElementById("problemeIndex");
@@ -143,6 +197,13 @@ function continuer(premiereFois = false) {
     if (probleme[indexProbleme] == null) {
         document.getElementById("problemeNumero").style.color = "green";
         textStatus.innerHTML = "Vous avez fait tous les problèmes";
+        if (note/nbProbleme >= 6) {
+            textStatus.innerHTML +=  "<br><br>Votre note : "+(note/nbProbleme)*100+"%";
+        }
+        if (note/nbProbleme < 6) {
+            textStatus.innerHTML +=  "<br><br>Votre note : "+(note/nbProbleme)*100+"% <br>Rechargez la page pour réessayer";
+        }
+        textStatus.innerHTML +=  "<br><br>Votre note : "+(note/nbProbleme)*100+"%"
         textStatus.style.color = "green";
         document.getElementById("form").style.display = "none";
         return null
@@ -161,4 +222,18 @@ function continuer(premiereFois = false) {
     choix3.checked = false;
 
     bonneReponseEntre = false;
+
+    choix1cliquer = false;
+    choix2cliquer = false;
+    choix3cliquer = false;
+    choix4cliquer = false;
+
+    label1.style.color = "wheat";
+    label1.innerHTML = label1textDefault;
+    label2.style.color = "wheat";
+    label2.innerHTML = label2textDefault;
+    label3.style.color = "wheat";
+    label3.innerHTML = label3textDefault;
+    label4.style.color = "wheat";
+    label4.innerHTML = label4textDefault;
 }
